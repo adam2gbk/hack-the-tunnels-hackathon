b5945f6ac34d59a1256615c4c2d43f34ed6f6151
adam <adam2gbk@gmail.com>
2024-10-19 15:35:29 -0400

    Challenge 1: Create Login Page

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -3,13 +3,20 @@ import { useAccountContext } from "../../context";
 import { Base as Layout } from "@/layouts";
 import "./Login.style.scss";
 
+// / "admin@email.com"
+// "password"
 function Login() {
-  const [message, setMessage] = useState(null);
+  const [message, setMessage] = useState<string | null>(null);
   const { login } = useAccountContext();
+  
+  const [profile, setProfile] = useState({
+    username: '',
+    password: '',
+  });
 
   const attemptLogin = async () => {
     try {
-      const message = await login("admin@email.com", "password");
+      const message = await login(profile.username, profile.password);
       setMessage(message);
     } catch (error) {
       console.log(error);
@@ -21,12 +28,12 @@ function Login() {
       <div className="Login"></div>
       <div className="Login__panel">
         <div className="Login__panel__content">
-          <img src="/carleton_logo_black.png"></img>
+          <img src="/carleton_logo_black.png" alt="Carleton Logo" />
           <div className="Login__panel__content__message">
             <div>Welcome to the Carleton SSO Federated Portal.</div>
             <div>
               Enter your{" "}
-              <a href="https://myone.carleton.ca" target="blank">
+              <a href="https://myone.carleton.ca" target="_blank" rel="noopener noreferrer">
                 MyCarletonOne
               </a>{" "}
               username and password.
@@ -34,16 +41,36 @@ function Login() {
           </div>
           {message && <p>{message}</p>}
           <div className="Login__panel__content__input">
-            <input type="text" placeholder="MyCarletonOne username"></input>
-            <input type="password" placeholder="Password"></input>
+            <input 
+              type="text" 
+              value={profile.username} 
+              onChange={e => {
+                setProfile({
+                  ...profile,
+                  username: e.target.value,
+                });
+              }} 
+              placeholder="MyCarletonOne username" 
+            />
+            <input 
+              type="password" 
+              value={profile.password} 
+              onChange={e => {
+                setProfile({
+                  ...profile,
+                  password: e.target.value,
+                });
+              }} 
+              placeholder="Password" 
+            />
           </div>
           <div className="Login__panel__content__checkbox">
-            <input type="checkbox"></input>
+            <input type="checkbox" />
             <label>Keep me signed in</label>
           </div>
           <button
             className="Login__panel__button"
-            onClick={() => attemptLogin()}
+            onClick={attemptLogin}
           >
             Sign In
           </button>
